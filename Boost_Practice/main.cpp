3a6ee47f4dc95a4a1fa5970530b78fda73436c40
진재 김 <rlawlswo6638@gmail.com>
2022-01-02 21:00:28 +0900

    update

--- Boost_Practice/main.cpp
+++ Boost_Practice/main.cpp
@@ -1,6 +1,8 @@
 // Boost_Practice_Client(속성) -> 링커 -> 일반 -> 추가 라이브러리 디렉터리 -> boost의 라이브러리(C:\boost_1_78_0\stage\lib) 추가
 // Boost_Practice_Client(속성) -> 구속 속성 -> VC++ 디렉터리 -> 포함 디렉터리 -> boost의 라이브러리(C:\boost_1_78_0\stage\lib)의 루트 디렉터리(C:\boost_1_78_0)를 추가
 
+// Client
+
 #include<iostream>
 #include<boost/asio.hpp>
 #include<boost/date_time/posix_time/posix_time.hpp>
